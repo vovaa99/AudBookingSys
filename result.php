17edb2473f8cb6d15c41195c417b835c23c5d289
vovaa99 <vovaa99@users.noreply.github.com>
2018-07-08 21:43:01 +0300

    install upd

--- result.php
+++ result.php
@@ -1,35 +1,32 @@
 <?php
-$err=0;
+
+$err = 0;
 session_start();
 session_destroy();
 
-    unlink('index.php');
-    rename('install/index.php', 'index.php');
+unlink('index.php');
+rename('install/index.php', 'index.php');
 
-    if (is_file('index.php') && filesize('index.php') != 13736){   ///вес install.php в байтах
-        print "<b>Замените файл index.php файлом install/index.php, удалите папку install, удалите файл result.php, чтобы завершить установку. </b><br>";   
-        $err++;
-    } elseif (!is_file('index.php')) {    
-        print "<b>Переместите файл  install/index.php в корневую директорию, удалите папку install и удалите файл result.php, чтобы завершить установку. </b><br>";   
-        $err++;
-    }
-    else {
-        unlink('install');
-        unlink('result.php');
-    }
-    if($err == 0 && is_dir('install')&& is_file('result.php'))
-    {        
-        print "<b>Удалите папку install и удалите файл result.php, чтобы завершить установку. </b><br>";
-        $err++;
-    }
-    if($err == 0 && is_dir('install')&& !is_file('result.php'))
-    {        
-        print "<b>Удалите папку install, чтобы завершить установку. </b><br>";
-        $err++;
-    }
-    if($err == 0)
-    {
-         print "<b>Установка прошла успешно.</b><br>";
-         sleep(30);
-         header('Loaction: index.php');
-    }
+if (is_file('index.php') && filesize('index.php') != 13746) {   ///вес install.php в байтах
+    print "<b>Замените файл index.php файлом install/index.php, удалите папку install, удалите файл result.php, чтобы завершить установку. </b><br>";
+    $err++;
+} elseif (!is_file('index.php')) {
+    print "<b>Переместите файл  install/index.php в корневую директорию, удалите папку install и удалите файл result.php, чтобы завершить установку. </b><br>";
+    $err++;
+} else {
+    unlink('install');
+    unlink('result.php');
+}
+if ($err == 0 && is_dir('install') && is_file('result.php')) {
+    print "<b>Удалите папку install и удалите файл result.php, чтобы завершить установку. </b><br>";
+    $err++;
+}
+if ($err == 0 && is_dir('install') && !is_file('result.php')) {
+    print "<b>Удалите папку install, чтобы завершить установку. </b><br>";
+    $err++;
+}
+if ($err == 0) {
+    print "<b>Установка прошла успешно.</b><br>";
+    sleep(30);
+    header('Loaction: index.php');
+}
